e1edc5910472532b2a63c700cdf37dff10338dc8
Mahdi Jafari <mahdi.jfri.79@gmail.com>
2023-02-26 01:55:24 +0330

    Add whatisit section content

--- components/WhatIsIt.vue
+++ components/WhatIsIt.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="whatisit main-container">
-    SALAM WHAT IS It
+    <div class="text-container">
+      <div class="wii-text-header">
+        WHAT IS IT?
+      </div>
+      <div class="wii-text-body text-uppercase">
+        MLC is a event held by university of Sharif (SUT) teaching assistants of AI course.
+        in this event we learn about some of the deeper aspects of AI and challenge our knowledge in them in a contest.
+        the winner will have some extra points in AI course and there might be some other prizes too!
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -16,6 +25,29 @@ export default {
   background-image: url("~/assets/backgrounds/WhatIsIt.svg#svgView(preserveAspectRatio(none))");
   background-repeat: no-repeat;
   background-size: 100% 100%;
+}
+
+.whatisit .text-container {
+  width: 100%;
+  height: 100%;
+  padding: 10% 50% 0 5%;
+  color: black;
+}
+
+.whatisit .wii-text-header {
+  font-family: "Avenir Next LT Pro", sans-serif;
+  font-size: 3rem;
+  letter-spacing: 0.5rem;
+  margin-bottom: 3%;
+  font-weight: 700;
+}
+
+.whatisit .wii-text-body {
+  font-family: Inter, sans-serif;
+  letter-spacing: 0.2rem;
+  font-size: 1.25rem;
+  line-height: 2rem;
+  font-weight: 300;
 }
 
 </style>
