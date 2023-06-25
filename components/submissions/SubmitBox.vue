d5eaf0c2c6f683ae20881405ecaca8bfe84fd4b5
Mahdi Jafari <mahdi.jfri.79@gmail.com>
2023-06-25 22:32:06 +0330

    Submit using google drive link

--- components/submissions/SubmitBox.vue
+++ components/submissions/SubmitBox.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="w-100 h-100">
+  <div
+    v-if="fileSubmit"
+    class="w-100 h-100"
+  >
     <label
       for="fileInput"
       class="w-100 h-100 outer-box"
@@ -33,6 +36,23 @@
       @change="$forceUpdate()"
     >
   </div>
+  <div v-else class="w-100 h-100 outer-box">
+    <div class="inner-box w-100 h-100">
+      <div class="font-weight-bold submit">
+        SUBMIT
+      </div>
+      <div class="drive-link-container">
+        <label class="mt-auto mb-auto mr-2" for="drive-url-input">URL:</label>
+        <b-input id="drive-url-input" v-model="driveLink" type="text" placeholder="Google drive link" class="drive-link" />
+        <div class="btn ml-auto btn-primary" @click.prevent="submitDriveLink()">
+          <div v-if="!loading">
+            Submit
+          </div>
+          <b-spinner v-else small />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -52,7 +72,9 @@ export default {
   data () {
     return {
       loading: false,
-      hover: false
+      hover: false,
+      fileSubmit: false,
+      driveLink: ''
     }
   },
   methods: {
@@ -98,6 +120,32 @@ export default {
             autoHideDelay: 5000
           })
         })
+    },
+    submitDriveLink () {
+      this.loading = true
+      this.$axios.post(
+        '/' + this.challengeName + '/' + this.phase + '/submit-drive/',
+        { url: this.driveLink },
+        { headers: { Authorization: this.$store.state.token.token } }
+      )
+        .then((response) => {
+          this.loading = false
+          this.$emit('submitted')
+          this.driveLink = ''
+          this.$bvToast.toast('Submitted using google drive.', {
+            title: 'Submission',
+            variant: 'success',
+            autoHideDelay: 5000
+          })
+        })
+        .catch((_) => {
+          this.loading = false
+          this.$bvToast.toast('Failed to submit using google drive. Check the link and permissions of the file.', {
+            title: 'Submission',
+            variant: 'danger',
+            autoHideDelay: 10000
+          })
+        })
     }
   }
 }
@@ -130,5 +178,14 @@ div.content {
 div.btn {
   width: 10%;
   height: 20%;
+}
+
+.drive-link-container {
+  display: flex;
+  flex-direction: row;
+}
+
+.drive-link {
+  width: 50%;
 }
 </style>
